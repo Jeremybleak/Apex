d892108695fa59eb7333fc025efd7cd96f91e809
Jeremy Bleak <Jeremybleak99@gmail.com>
2021-03-04 17:08:33 -0700

    header added to the app

--- apex/src/App.js
+++ apex/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
+import Header from './Header';
 
 export default function App() {
   return (
     <div>
-      
+      <Header />
     </div>
   )
 }
