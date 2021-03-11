99d8b49f577f1eb302bddad4e2be9f6fc57f72bc
Jeremy Bleak <Jeremybleak99@gmail.com>
2021-03-10 18:11:17 -0700

    adding simple header items

--- apex/src/Header.js
+++ apex/src/Header.js
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Header() {
     return (
         <div>
-            
+            <img src="" alt=""/>
+            <div>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
         </div>
     )
 }
