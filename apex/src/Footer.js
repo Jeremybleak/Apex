eb68f844dfb27bbda975580a070b5bc8173c7d90
Jeremy Bleak <Jeremybleak99@gmail.com>
2021-03-18 21:04:16 -0600

    adding footer component

---
+++ apex/src/Footer.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+export default function Footer(){
+    return(
+        <div>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+    )
+}
